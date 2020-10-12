b4611c94ba8563cfa4c32f97f8bb88eff09cf434
Ibrahim Dauda <ibrolive@gmail.com>
2020-10-12 18:50:58 +0400

    added all required files

---
+++ README.md
@@ -0,0 +1,26 @@
+# terraform-rds-postgres-demo
+Terraform script to provision postgres database.
+
+## How to use
+### Create database password file
+#### On windows
+```
+mkdir secrets
+echo|set /p="Your_DB_Password" > secrets/postgresdb-password.txt
+```
+
+#### On Linux
+```
+mkdir secrets
+printf "Your_DB_Password" > secrets/postgresdb-password.txt
+```
+
+### Execute IaC scripts
+```
+terraform init
+terraform plan
+terraform apply
+terraform destroy
+```
+
+If you're looking for a simpler version see - https://blog.faraday.io/how-to-create-an-rds-instance-with-terraform/
---
+++ main.tf
@@ -0,0 +1,36 @@
+# Database Instance Cheatsheet:
+provider "aws" {
+  #version = "~> 2.0"
+  region = var.region
+}
+
+
+##################################################################################
+# DB instance
+##################################################################################
+resource "aws_db_instance" "sqlexpress" {
+  allocated_storage         = 20 # gigabytes
+  backup_retention_period   = 7   # in days
+  db_subnet_group_name      = aws_db_subnet_group.default.name
+  engine                    = "sqlserver-ex"
+  engine_version            = "14.00.1000.169.v1"
+  instance_class            = "db.t2.medium"
+  identifier                = "sqlexpress"
+  multi_az                  = false
+  name                      = null
+  create_db_parameter_group = false
+  timezone                  = "Central Standard Time"
+  license_model             = "license-included"
+  password                  = "${trimspace(file("${path.module}/secrets/postgresdb-password.txt"))}"
+  port                      = 1433
+  publicly_accessible       = true
+  storage_encrypted         = true # you should always do this
+  storage_type              = "gp2"
+  username                  = "sqlexpress"
+  vpc_security_group_ids    = ["${aws_security_group.AWS_VPC_Security_Group.id}"]
+  maintenance_window        = "Mon:00:00-Mon:03:00"
+  backup_window             = "03:00-06:00"
+  final_snapshot_identifier = "demodb" # Snapshot name upon DB deletion
+  major_engine_version      = "14.00"
+  deletion_protection       = false # Database Deletion Protection
+}
